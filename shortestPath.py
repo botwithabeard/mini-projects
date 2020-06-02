9aca97628fcef987e265c1d2108de62a67316f0b
botwithabeard <botwithabeard@gmail.com>
2020-06-02 10:08:31 +0530

    Shortest Path

--- shortestPath.py
+++ shortestPath.py
@@ -2,74 +2,55 @@ class Node(object):
     def __init__(self, name):
         """Assumes name is a string"""
         self.name = name
-
-        def getName(self):
-            return self.name
-
-        def __str__(self):
-            return self.name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
 
 class Edge(object):
     def __init__(self, src, dest):
-    """Assumes src and dest are nodes"""
-    self.src = src
-    self.dest = dest
-
+        """Assumes src and dest are nodes"""
+        self.src = src
+        self.dest = dest
     def getSource(self):
         return self.src
-
     def getDestination(self):
         return self.dest
-
     def __str__(self):
         return self.src.getName() + '->' + self.dest.getName()
 
-# class WeightedEdge(Edge):
-#     def __init__(self, src, dest, weight = 1.0):
-#     """Assumes src and dest are nodes, weight a number"""
-#     self.src = src
-#     self.dest = dest
-#     self.weight = weight
-#
-#     def getWeight(self):
-#     return self.weight
-#
-#     def __str__(self):
-#     return self.src.getName() + '->(' + str(self.weight) + ')' + self.dest.getName()
-
 class Digraph(object):
-    #nodes is a list of the nodes in the graph
-    #edges is a dict mapping each node to a list of its children
+    """edges is a dict mapping each node to a list of
+    its children"""
     def __init__(self):
-    self.nodes = []
-    self.edges = {}
-
+        self.edges = {}
     def addNode(self, node):
-        if node in self.nodes:
+        if node in self.edges:
             raise ValueError('Duplicate node')
         else:
-            self.nodes.append(node)
             self.edges[node] = []
-
     def addEdge(self, edge):
         src = edge.getSource()
         dest = edge.getDestination()
-        if not (src in self.nodes and dest in self.nodes):
+        if not (src in self.edges and dest in self.edges):
             raise ValueError('Node not in graph')
-            self.edges[src].append(dest)
-
+        self.edges[src].append(dest)
     def childrenOf(self, node):
         return self.edges[node]
-
     def hasNode(self, node):
-        return node in self.nodes
-
+        return node in self.edges
+    def getNode(self, name):
+        for n in self.edges:
+            if n.getName() == name:
+                return n
+        raise NameError(name)
     def __str__(self):
         result = ''
-        for src in self.nodes:
+        for src in self.edges:
             for dest in self.edges[src]:
-                result = result + src.getName() + '->'+ dest.getName() + '\n'
-        return result[:-1]
+                result = result + src.getName() + '->'\
+                         + dest.getName() + '\n'
+        return result[:-1] #omit final newline
 
 class Graph(Digraph):
     def addEdge(self, edge):
