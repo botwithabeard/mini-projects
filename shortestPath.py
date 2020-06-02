83621531306263d0a58075efe4eb794e887d5927
botwithabeard <botwithabeard@gmail.com>
2020-06-02 18:05:28 +0530

    shortest path problem

--- shortestPath.py
+++ shortestPath.py
@@ -24,7 +24,7 @@ class Digraph():
     its children"""
     def __init__(self):
         self.edges = {}
-        
+
     def addNode(self, node):
         if node in self.edges:
             raise ValueError('Duplicate node')
@@ -55,10 +55,78 @@ class Digraph():
             for dest in self.edges[src]:
                 result = result + src.getName() + '->'\
                          + dest.getName() + '\n'
-        return result[:-1] #omit final newline
+        return result[:-1]
 
 class Graph(Digraph):
     def addEdge(self, edge):
         Digraph.addEdge(self, edge)
         rev = Edge(edge.getDestination(), edge.getSource())
         Digraph.addEdge(self, rev)
+
+def buildCityGraph(graphType):
+    g = graphType()
+    for name in ('Boston', 'Providence', 'New York', 'Chicago',
+                 'Denver', 'Phoenix', 'Los Angeles'):
+        g.addNode(Node(name))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('Providence')))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Boston')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
+    return g
+
+
+def printPath(path):
+    """Assumes path is a list of nodes"""
+    result = ''
+    for i in range(len(path)):
+        result = result + str(path[i])
+        if i != len(path) - 1:
+            result = result + '->'
+    return result
+
+def DFS(graph, start, end, path, shortest, toPrint = False):
+    """Assumes graph is a Digraph; start and end are nodes;
+          path and shortest are lists of nodes
+       Returns a shortest path from start to end in graph"""
+    path = path + [start]
+    if toPrint:
+        print('Current DFS path:', printPath(path))
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path:
+            if shortest == None or len(path) < len(shortest):
+                newPath = DFS(graph, node, end, path, shortest,
+                              toPrint)
+                if newPath != None:
+                    shortest = newPath
+        elif toPrint:
+            print('Already visited', node)
+    return shortest
+
+def shortestPath(graph, start, end, toPrint = False):
+    """Assumes graph is a Digraph; start and end are nodes
+       Returns a shortest path from start to end in graph"""
+    return DFS(graph, start, end, [], None, toPrint)
+
+def testSP(source, destination):
+    g = buildCityGraph(Digraph)
+    sp = shortestPath(g, g.getNode(source), g.getNode(destination),
+                      toPrint = True)
+    if sp != None:
+        print('Shortest path from', source, 'to',
+              destination, 'is', printPath(sp))
+    else:
+        print('There is no path from', source, 'to', destination)
+
+if __name__ == '__main__':
+    testSP('Boston', 'New York')
+    print()
+    testSP('Boston', 'Phoenix')
+    print()
