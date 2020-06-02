8029f0ff4ed51367bee97d1d940629556e5b3a72
botwithabeard <botwithabeard@gmail.com>
2020-06-02 13:18:21 +0530

    Shortest Path

--- shortestPath.py
+++ shortestPath.py
@@ -1,4 +1,4 @@
-class Node(object):
+class Node():
     def __init__(self, name):
         """Assumes name is a string"""
         self.name = name
@@ -7,7 +7,7 @@ class Node(object):
     def __str__(self):
         return self.name
 
-class Edge(object):
+class Edge():
     def __init__(self, src, dest):
         """Assumes src and dest are nodes"""
         self.src = src
@@ -19,31 +19,36 @@ class Edge(object):
     def __str__(self):
         return self.src.getName() + '->' + self.dest.getName()
 
-class Digraph(object):
+class Digraph():
     """edges is a dict mapping each node to a list of
     its children"""
     def __init__(self):
         self.edges = {}
+        
     def addNode(self, node):
         if node in self.edges:
             raise ValueError('Duplicate node')
-        else:
-            self.edges[node] = []
+        self.edges[node] = []
+
     def addEdge(self, edge):
         src = edge.getSource()
         dest = edge.getDestination()
         if not (src in self.edges and dest in self.edges):
             raise ValueError('Node not in graph')
         self.edges[src].append(dest)
+
     def childrenOf(self, node):
         return self.edges[node]
+
     def hasNode(self, node):
         return node in self.edges
+
     def getNode(self, name):
         for n in self.edges:
             if n.getName() == name:
                 return n
         raise NameError(name)
+
     def __str__(self):
         result = ''
         for src in self.edges:
